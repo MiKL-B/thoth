9a3dabccd8dcf20ff3992f10a3d2d00b0bb562b2
MiKL-B <michaelbecquer@protonmail.com>
2025-06-30 11:54:53 +0200

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { WorkspaceComponent } from './pages/workspace/workspace.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  {
+    path:"workspace",
+    component:WorkspaceComponent,
+     canActivate:[authGuard],
+    //   children: [
+    //   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    //   { path: 'dashboard', component: DashboardComponent },
+    //   { path: 'notes', component: NotesComponent },
+    //   { path: 'kanban', component: KanbanComponent },
+    //   { path: 'calendar', component: CalendarComponent },
+    //   { path: '**', component: NotFoundComponent } // en option ici
+    // ]
+  },
+  {
+    path:"**", component:NotFoundComponent
+  }
 ];
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, Menu } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  readonly Menu = Menu;
+}
