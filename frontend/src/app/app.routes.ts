581312e0b8d8695cd86f6bc7886ba8256b7b667d
MiKL-B <michaelbecquer@protonmail.com>
2025-06-30 13:00:05 +0200

    refactor archi folder

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { WorkspaceComponent } from './pages/workspace/workspace.component';
-import { authGuard } from './core/guards/auth.guard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
