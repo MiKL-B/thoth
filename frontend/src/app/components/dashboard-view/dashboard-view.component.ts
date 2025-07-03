a582a43b26568062451a4a8fd7ff28a661f408b3
MiKL-B <michaelbecquer@protonmail.com>
2025-07-03 15:54:09 +0200

    update

--- frontend/src/app/components/dashboard-view/dashboard-view.component.ts
+++ frontend/src/app/components/dashboard-view/dashboard-view.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LucideIconComponent } from "../shared/lucide-icon/lucide-icon.component";
 
 @Component({
   selector: 'app-dashboard-view',
-  imports: [],
+  imports: [LucideIconComponent],
   templateUrl: './dashboard-view.component.html',
   styleUrl: './dashboard-view.component.scss'
 })
