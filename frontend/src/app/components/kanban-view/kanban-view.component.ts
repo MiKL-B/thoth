554d1e7ce80e8cb8f038ae5927503339539b9177
MiKL-B <michaelbecquer@protonmail.com>
2025-07-09 15:34:14 +0200

    update

--- frontend/src/app/components/kanban-view/kanban-view.component.ts
+++ frontend/src/app/components/kanban-view/kanban-view.component.ts
@@ -1,69 +1,12 @@
 import { Component } from '@angular/core';
-import { NoteListItemComponent } from "../note-list-item/note-list-item.component";
-import { Note } from '../../models/note.model';
+
 
 @Component({
   selector: 'app-kanban-view',
-  imports: [NoteListItemComponent],
+  imports: [],
   templateUrl: './kanban-view.component.html',
   styleUrl: './kanban-view.component.scss'
 })
 export class KanbanViewComponent {
-  notes: Note[] = [
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-    {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    }, {
-      id: 0,
-      title: "Note test",
-      content: "",
-      pinned: false,
-      notebookID: 1
-    },
-  ]
+
 }
