d8217c8265b3b0b7d443574c95dd9702d6e47799
MiKL-B <michaelbecquer@protonmail.com>
2025-07-09 17:38:38 +0200

    fix ternary condition lucide icon

--- frontend/src/app/components/shared/lucide-icon/lucide-icon.component.ts
+++ frontend/src/app/components/shared/lucide-icon/lucide-icon.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit, } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { ICONS } from '../../../lucide-icon';
 @Component({
   selector: 'app-lucide-icon',
   standalone: true,
   imports: [LucideAngularModule],
-  template: '<lucide-icon [name]="iconName" [size]="size" [color]="color"></lucide-icon>',
+  template: '<lucide-icon [name]="name" [size]="size" [color]="color"></lucide-icon>',
   styleUrl: './lucide-icon.component.scss'
 })
-export class LucideIconComponent implements OnInit {
+export class LucideIconComponent implements OnChanges {
 
   @Input({ required: true }) name!: string;
   @Input() size!: number;
@@ -16,9 +16,11 @@ export class LucideIconComponent implements OnInit {
   iconName = 'HelpCircle';
 
 
-  ngOnInit(): void {
-    const foundIcon = ICONS.find(i => i.name.toLowerCase() === this.name.toLowerCase());
-    if (!foundIcon) return;
-    this.iconName = foundIcon.name ?? this.iconName
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['name']) {
+      const foundIcon = ICONS.find(i => i.name.toLowerCase() === this.name.toLowerCase());
+      if (!foundIcon) return;
+      this.name = foundIcon.name ?? this.name
+    }
   }
 }
