21aff5dae8e413f938ea2d2af0c423575d24db5f
MiKL-B <michaelbecquer@protonmail.com>
2025-07-12 13:18:29 +0200

    connect api

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
-
+  message = "";
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('http://localhost:8080/api/hello', { responseType: 'text' })
+      .subscribe(data => this.message = data);
+  }
 }
