722e3c4f0fa098db142179f1abed57811f4fba5b
MiKL-B <michaelbecquer@protonmail.com>
2025-07-16 16:38:23 +0200

    update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { WorkspaceComponent } from './pages/workspace/workspace.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -10,16 +9,8 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: "workspace",
-    component: WorkspaceComponent,
+    loadComponent: () => import("./pages/workspace/workspace.component").then((c) => c.WorkspaceComponent),
     canActivate: [authGuard],
-    //   children: [
-    //   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    //   { path: 'dashboard', component: DashboardComponent },
-    //   { path: 'notes', component: NotesComponent },
-    //   { path: 'kanban', component: KanbanComponent },
-    //   { path: 'calendar', component: CalendarComponent },
-    //   { path: '**', component: NotFoundComponent } // en option ici
-    // ]
   },
   {
     path: "**", component: NotFoundComponent
--- frontend/src/app/pages/not-found/not-found.component.ts
+++ frontend/src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../components/shared/header/header.component';
-import { FooterComponent } from '../../components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-not-found',
-  imports: [HeaderComponent,FooterComponent],
+  imports: [],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
